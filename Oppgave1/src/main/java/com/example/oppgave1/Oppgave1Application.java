3b7e8992844093cf84240f1af21d06090ade114c
Omer-Gndz <omrgndz56@gmail.com>
2023-02-16 16:33:03 +0100

    16.02.2023

---
+++ Oppgave1/src/main/java/com/example/oppgave1/Oppgave1Application.java
@@ -0,0 +1,13 @@
+package com.example.oppgave1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oppgave1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oppgave1Application.class, args);
+	}
+
+}
---
+++ Oppgave1/src/test/java/com/example/oppgave1/Oppgave1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oppgave1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oppgave1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
